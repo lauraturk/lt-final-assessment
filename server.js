6bfb0cda7fafb0974e9e51c9f61311310b1310b7
Laura Turk <laura.lk.turk@gmail.com>
2017-07-27 15:35:07 -0600

    initial front end add components

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const moment = require('moment')
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
